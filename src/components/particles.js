b91935d5dc06757408c9259366164030f628726a
yanhackcode15 <iamyanhu@gmail.com>
2024-04-11 16:31:39 -0700

    stil working on projects page

---
+++ src/components/particles.js
@@ -0,0 +1,216 @@
+import React from "react";
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {loadPolygonMaskPlugin} from "@tsparticles/plugin-polygon-mask";
+import { loadSlim } from "@tsparticles/slim";
+import deerSVG from '../assets/images/smalldeer.svg';
+import profileSVG from '../assets/images/profilePic.svg'
+
+export function ParticlesContainer() {
+    const [init, setInit] = useState(false);
+    const deerurl = "https://particles.js.org/images/smalldeer.svg"
+
+  // this should be run only once per application lifetime
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            await loadSlim(engine);
+            await loadPolygonMaskPlugin(engine);
+        }).then(() => {
+            setInit(true);
+        }).catch((error) => {
+            console.error("Particles Engine initialization failed:", error);
+        })
+    }, []);
+
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
+    const options = useMemo(
+        ()=> ({
+            fpsLimit: 20,
+            autoPlay: true,
+            fullScreen: {
+                enable: true,
+                zIndex: 0
+            },
+            particles: {
+                bounce: {
+                    horizontal: {
+                        value: 1
+                    },
+                    verticle: {
+                        value: 1
+                    }
+                },
+                collisions: {
+                    effect: {
+                        close: true,
+                        fill: true,
+                        options: {},
+                        type: {}
+                    },
+                    absorb: {
+                        speed: 2
+                    },
+                    bounce: {
+                        horizontal: {
+                            value: 1
+                        },
+                        verticle: {
+                            value: 1
+                        }
+                    },
+                    enable: false,
+                    maxSpeed: 50,
+                    mode: "bounce",
+                    overlap: {
+                        enable: true,
+                        retries: 0
+                    }
+                },
+                number: {
+                    value: 220,
+                    density: {
+                        enable: true,
+                        value_area: 8000
+                    }
+                },
+                links: {
+                    enable: true,
+                    distance: 130,
+                    color: "#ffffff",
+                    opacity: 0.7,
+                    width: 1
+                },
+                color: {
+                    value: "#ffffff"
+                },
+                opacity: {
+                    value: 0.5,
+                    anim: {
+                        enable: true
+                    }
+                },
+                size: {
+                    value: 3,
+                    random: true,
+                    anim: {
+                        enable: true,
+                        speed: 3
+                    }
+                },
+                move: {
+                    angle: {
+                        offset: 0,
+                        value: 90
+                      },
+                      attract: {
+                        distance: 200,
+                        enable: false,
+                        rotate: {
+                          x: 3000,
+                          y: 3000
+                        }
+                      },
+                      center: {
+                        x: 150,
+                        y: 50,
+                        mode: "percent",
+                        radius: 0
+                      },
+                      decay: 0,
+                      distance: {},
+                      direction: "none",
+                      drift: 0,
+                      enable: true,
+                      gravity: {
+                        acceleration: 9.81,
+                        enable: false,
+                        inverse: false,
+                        maxSpeed: 50
+                      },
+                      path: {
+                        clamp: true,
+                        delay: {
+                          value: 0
+                        },
+                        enable: false,
+                        options: {}
+                      },
+                      outModes: {
+                        default: "bounce",
+                        bottom: "bounce",
+                        left: "bounce",
+                        right: "bounce",
+                        top: "bounce"
+                      },
+                      random: false,
+                      size: false,
+                      speed: 1,
+                      spin: {
+                        acceleration: 0,
+                        enable: false
+                      },
+                      straight: false,
+                      trail: {
+                        enable: false,
+                        length: 10,
+                        fill: {}
+                      },
+                      vibrate: false,
+                      warp: false
+                },
+            },
+            polygon: {
+                position: {
+                    x: 92,
+                    y: 70
+                },
+                enable: true,
+                type: "inline", // "inline" for using SVG path
+                url: profileSVG, // Ensure this is the correct path to your SVG
+                scale: -0.3, // Adjust as needed
+                inline: {
+                  arrangement: "equidistant"
+                },
+                move: {
+                  radius: 50,
+                  type: "path"
+                },
+                draw: {
+                  enable: true,
+                  stroke: {
+                    color: "rgba(255, 255, 255, 1)" // Adjust the color as needed
+                  }
+                }
+            },
+            interactivity: {
+                detectsOn: 'window',
+                events: {
+                    onHover: {
+                        enable: true,
+                        mode: "bubble"
+                    }
+                },
+                modes: {
+                    bubble: {
+                        size: 60,
+                        distance: 40
+                    }
+                }
+            }
+        }),[]
+    )
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    );
+  }
+
+  return <></>;
+}
---
+++ src/content_option.js
@@ -0,0 +1,168 @@
+import bgImage from "./assets/images/bgImage.jpg"
+const logotext = "YH";
+const meta = {
+    title: "Yan Hu",
+    description: "I’m Yan Hu, Full stack devloper,currently working in LA",
+};
+
+const introdata = {
+    title: "I’m Yan Hu",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+    },
+    description: "Full Stack Dev with a Passion for Problem-Solving | Node JS, Python, React, Data Analytics | Experienced Product Manager & Entrepreneur",
+    your_img: bgImage,
+};
+
+const dataabout = {
+    title: "about me",
+    aboutme: "Hi there! I'm pursuing a Node.js | React full-stack dev role to create innovative products. My career path has been diverse - with over a decade working in both startups and mature companies as a product lead, followed by the most recent 5 years as an entrepreneur, where I have built a thriving business from scratch, to now where I’m keenly focused on pursuing a full stack developer role.",
+};
+const worktimeline = [{
+        jobtitle: "Full Stack Developer",
+        where: "Self Employed",
+        date: "2017 - Present",
+    },
+    {
+        jobtitle: "Owner/Operator",
+        where: "Snip-its Kids Hair Salon",
+        date: "2016 - Present",
+    },
+    {
+        jobtitle: "Senior Product Manager",
+        where: "RentPath/Lovely",
+        date: "2014 - 2016",
+    },
+    {
+        jobtitle: "Director of Ecommerce",
+        where: "JustFab",
+        date: "2012 - 2013",
+    },
+    {
+        jobtitle: "Senior Product Manager",
+        where: "Hautelook, A Nordstrom Company",
+        date: "2011 - 2012",
+    },
+    {
+        jobtitle: "Product Manager",
+        where: "Live Nation Entertainment",
+        date: "2006 - 2011",
+    },
+];
+
+const skills = [{
+        name: "Laungage:",
+        value: "Node JS, Python, Typescript, HTML, CSS",
+    },
+    {
+        name: "APIs/Libraries:",
+        value: "React, Bootstrap, Firebase, Twilio Programmable Voice, Twilio SMS, Alexa API, OpenAI API",
+    },
+    {
+        name: "Design Tools",
+        value: "Figma, Photoshoop, Canva",
+    },
+    {
+        name: "Dev Ops",
+        value: "Git, Heroku, VS Code",
+    },
+    {
+        name: "Others",
+        value: "Slack, Jira, Trello, Kanban, Scrum",
+    },
+];
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "xxxxxx",
+    link: "",
+}]
+// const dataportfolio = [{
+//         img: "https://picsum.photos/400/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/800/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/600/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/300/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/700/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+
+//     {
+//         img: "https://picsum.photos/400/600/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/300/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/550/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+//     {
+//         img: "https://picsum.photos/400/700/?grayscale",
+//         description: "The wisdom of life consists in the elimination of non-essentials.",
+//         link: "#",
+//     },
+// ];
+
+const contactConfig = {
+    YOUR_EMAIL: "ibuildcoolthings@gmail.com",
+    YOUR_FONE: "(310)876-9607",
+    description: "I would love to connect with you",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/yanhackcode15",
+    linkedin: "https://linkedin.com/in/yanhu1",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
